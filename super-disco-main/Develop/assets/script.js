1fe739975dd2a28368574c076d64e8039a51f33a
scottskinn <84105761+scottskinn@users.noreply.github.com>
2021-06-24 14:04:35 -0400

    time blocks added

--- super-disco-main/Develop/assets/script.js
+++ super-disco-main/Develop/assets/script.js
@@ -7,4 +7,19 @@ currentDay.textContent = (currentTime.format('LLLL'));
 console.log(currentDay);
 header.appendChild(currentDay);
 
+// assign savebtn with click listener
+$(".savebtn").on("click", function(){
+    console.log(this)
+})
 
+
+
+$("#hour9 .description").val(localStorage.getItem("hour9"));
+$("#hour10 .description").val(localStorage.getItem("hour10"));
+$("#hour11 .description").val(localStorage.getItem("hour11"));
+$("#hour12 .description").val(localStorage.getItem("hour12"));
+$("#hour13 .description").val(localStorage.getItem("hour13"));
+$("#hour14 .description").val(localStorage.getItem("hour14"));
+$("#hour15 .description").val(localStorage.getItem("hour15"));
+$("#hour16 .description").val(localStorage.getItem("hour16"));
+$("#hour17 .description").val(localStorage.getItem("hour17"));
