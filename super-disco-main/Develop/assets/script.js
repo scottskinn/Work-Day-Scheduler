48caa9cbeda196fa75ac5e7df7a5c92c0ded1359
scottskinn <84105761+scottskinn@users.noreply.github.com>
2021-06-25 13:18:30 -0400

    local storage undefined

--- super-disco-main/Develop/assets/script.js
+++ super-disco-main/Develop/assets/script.js
@@ -1,17 +1,20 @@
 // setting the date in the header
 var header = document.querySelector(".jumbotron")
-var currentDay = document.createElement("currentDay")
+var currentDay = document.querySelector("#currentDay")
+var description = document.querySelector(".description")
 
 var currentTime = moment();
 currentDay.textContent = (currentTime.format('LLLL'));
-console.log(currentDay);
 header.appendChild(currentDay);
 
 // assign savebtn with click listener
-$(".savebtn").on("click", function(){
-    console.log(this)
-})
+$(".container .savebtn").on("click", function(){
+    console.log(localStorage);
 
+    var text = $(this).siblings(".description").value;  
+    var time = $(this).parent().attr("id");
+
+    localStorage.setItem(time, text);
 
 
 $("#hour9 .description").val(localStorage.getItem("hour9"));
@@ -23,3 +26,30 @@ $("#hour14 .description").val(localStorage.getItem("hour14"));
 $("#hour15 .description").val(localStorage.getItem("hour15"));
 $("#hour16 .description").val(localStorage.getItem("hour16"));
 $("#hour17 .description").val(localStorage.getItem("hour17"));
+});
+
+
+// Set each timeblock in the daily schedule using moment.js
+var hour9 = moment().hour(9);
+var hour10 = moment().hour(10);
+var hour11 = moment().hour(11);
+var hour12 = moment().hour(12);
+var hour13 = moment().hour(13);
+var hour14 = moment().hour(14);
+var hour15 = moment().hour(15);
+var hour16 = moment().hour(16);
+var hour17 = moment().hour(17);
+
+// change the time block color based on hour
+function hourTracker() {
+    var currentHour = moment().format('H');
+
+    if (currentHour === (hour9)) {
+        $("#hour9").addClass("present");
+        } else {        
+        $("#hour9").addClass("future");
+        $("#hour9").addClass("past");
+    }
+
+    hourTracker();
+};
